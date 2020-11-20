49f5faaac5c839e3b1adf9271eb56785ec4b1531
AndrewMorrow <beardedmongo@gmail.com>
2020-11-19 20:55:09 -0600

    fixing writefile

--- server.js
+++ server.js
@@ -6,27 +6,31 @@ var PORT = process.env.PORT || 8000;
 var db = require("./db/db.json");
 
 // middleware
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
 // routes
-app.get("/hello", function (req, res) {
-    res.send("hello");
-});
 
-app.get("/notes", function (req, res) {
+// app.get("/", (req, res) => {
+//     // res.send();
+// });
+
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"), function (err) {
         if (err) throw err;
     });
 });
 
-app.post("/api/notes", function (req, res) {
+app.post("/api/notes", (req, res) => {
     var note = req.body;
-    console.log({ note });
+    // console.log(req.body);
+    // console.log(req.body.title);
+    // console.log({ note });
     db.push(note);
     console.log({ db });
-    fs.writeFileSync("./db/db.json", toString(db));
+    // fs.writeFileSync("./db/db.json", db.toString());
 });
 
 //listener
